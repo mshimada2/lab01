d3d188f445113d9c9a89525b0338705cd2bc51fe
mshimada <mshimada2@mail.csuchico.edu>
2018-08-28 14:23:21 -0700

    finished

---
+++ print/print.cpp
@@ -0,0 +1,23 @@
+//Author: McDermott Liam Shimada 
+
+#include<iostream> 
+
+using namespace std;
+
+int main()
+{
+
+  int value;
+
+
+  cout<<"Please enter how many times you want the program to say hello. "<<endl;
+  cin>>value; 
+
+  for(int x = 0; x < value; x++)
+  {
+    cout<<"hello"<<endl;
+
+  }
+
+}
+
