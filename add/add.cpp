4a12c925bae7d2af0c0ee3aeaf32f46bc892ec48
mshimada <mshimada2@mail.csuchico.edu>
2018-08-28 14:22:36 -0700

    hjkhjahk

---
+++ add/add.cpp
@@ -0,0 +1,23 @@
+//Author McDermott Liam Shimada 
+
+#include<iostream> 
+
+using namespace std;
+
+int main()
+{
+
+  int value1;
+  int value2;
+  int answer;
+
+  cout<<"Please enter two numbers to add together. "<<endl;
+  cin>>value1;
+  cin>>value2;
+
+  answer = value1 + value2;
+
+  cout<<value1<<" + "<<value2<<" = "<<answer<<endl;
+
+}
+
